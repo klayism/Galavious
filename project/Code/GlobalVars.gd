8cd7fa76fb6ca23cb7745d4cf8d881df7592bccb
klayism <94929502+klayism@users.noreply.github.com>
2024-01-18 16:28:04 -0500

    significantly shortened the code
    made FiringTimer have it's own function to make itself

--- project/Code/GlobalVars.gd
+++ project/Code/GlobalVars.gd
@@ -1,7 +1,6 @@
 extends Node
 
 var PlayerPos
-var inieneins
 var score = 0
 var enemyCount = 0
 
--- project/Code/Main.gd
+++ project/Code/Main.gd
@@ -56,71 +56,64 @@ func _on_mob_spawn_timeout(): # there has got to be a better way to do this
 		var scene = preload("res://Scenes/Enemy.tscn")
 		enemy_instance = scene.instantiate()
 		enemy_instance.add_to_group("ToReset")
-		var FiringTimer = Timer.new()
 		spawnplacement(enemy_instance, random_number)
-		FiringTimer.timeout.connect(fire.bind(FiringTimer,enemy_instance.position,enemy_instance,enemycoolcount))
-		FiringTimer.wait_time = 0.7
-		FiringTimer.autostart = true
-		enemy_instance.add_child(FiringTimer)
+		var letimer = firingtimermaker(enemy_instance, enemycoolcount)
+		enemy_instance.add_child(letimer)
 		$MobSpawnTimer.start()
-		GlobalVars.inieneins = FiringTimer
 		enemycoolcount += 1
 	elif enemycoolcount == 2:
 		var random_number = randi() % 9
 		var scene2 = preload("res://Scenes/Enemy.tscn")
 		enemy_instance2 = scene2.instantiate()
 		enemy_instance2.add_to_group("ToReset")
-		var FiringTimer2 = Timer.new()
 		spawnplacement(enemy_instance2, random_number)
-		FiringTimer2.timeout.connect(fire.bind(FiringTimer2,enemy_instance2.position,enemy_instance2,enemycoolcount))
-		FiringTimer2.wait_time = 1.1
-		FiringTimer2.autostart = true
-		enemy_instance2.add_child(FiringTimer2)
+		var letimer = firingtimermaker(enemy_instance2,enemycoolcount)
+		enemy_instance2.add_child(letimer)
 		$MobSpawnTimer.start()
-		GlobalVars.inieneins = FiringTimer2
 		enemycoolcount += 1
 	elif enemycoolcount == 3:
 		var random_number = randi() % 9
 		var scene3 = preload("res://Scenes/Enemy.tscn")
 		enemy_instance3 = scene3.instantiate()
 		enemy_instance3.add_to_group("ToReset")
-		var FiringTimer3 = Timer.new()
 		spawnplacement(enemy_instance3, random_number)
-		FiringTimer3.timeout.connect(fire.bind(FiringTimer3,enemy_instance3.position,enemy_instance3,enemycoolcount))
-		FiringTimer3.wait_time = 1.1
-		FiringTimer3.autostart = true
-		enemy_instance3.add_child(FiringTimer3)
+		var letimer = firingtimermaker(enemy_instance2,enemycoolcount)
+		enemy_instance3.add_child(letimer)
 		$MobSpawnTimer.start()
-		GlobalVars.inieneins = FiringTimer3
 		enemycoolcount += 1
 	elif enemycoolcount == 4:
 		var random_number = randi() % 9
 		var scene4 = preload("res://Scenes/Enemy.tscn")
 		enemy_instance4 = scene4.instantiate()
 		enemy_instance4.add_to_group("ToReset")
-		var FiringTimer4 = Timer.new()
 		spawnplacement(enemy_instance4, random_number)
-		FiringTimer4.timeout.connect(fire.bind(FiringTimer4,enemy_instance4.position,enemy_instance4,enemycoolcount))
-		FiringTimer4.wait_time = 1.1
-		FiringTimer4.autostart = true
-		enemy_instance4.add_child(FiringTimer4)
+		var letimer = firingtimermaker(enemy_instance2,enemycoolcount)
+		enemy_instance4.add_child(letimer)
 		$MobSpawnTimer.start()
-		GlobalVars.inieneins = FiringTimer4
 		enemycoolcount += 1
 	elif enemycoolcount == 5:
 		var random_number = randi() % 9
 		var scene5 = preload("res://Scenes/Enemy.tscn")
 		enemy_instance5 = scene5.instantiate()
 		enemy_instance5.add_to_group("ToReset")
-		var FiringTimer5 = Timer.new()
 		spawnplacement(enemy_instance5, random_number)
-		FiringTimer5.timeout.connect(fire.bind(FiringTimer5,enemy_instance5.position,enemy_instance5,enemycoolcount))
-		FiringTimer5.wait_time = 1.1
-		FiringTimer5.autostart = true
-		enemy_instance5.add_child(FiringTimer5)
+		var letimer = firingtimermaker(enemy_instance2,enemycoolcount)
+		enemy_instance5.add_child(letimer)
 		$MobSpawnTimer.start()
-		GlobalVars.inieneins = FiringTimer5
 		enemycoolcount = 1
+
+func firingtimermaker(enemy_instance, enemycoolcount):
+	var FiringTimer = Timer.new()
+	var firingtimerpath = FiringTimer.get_path()
+	if get_node_or_null(firingtimerpath):
+		FiringTimer.timeout.connect(fire.bind(FiringTimer,enemy_instance.position,enemy_instance,enemycoolcount))
+		FiringTimer.wait_time = 0.7
+		FiringTimer.autostart = true
+		return FiringTimer
+	else:
+		FiringTimer.queue_free()
+
+
 func fire(selfname, inspos, parentenemy,enemycoolcountusehere): # there has got to be a better way to do this 
 	if enemycoolcountusehere == 1:
 		
@@ -233,6 +226,8 @@ func spawnplacement(instance, random_number):
 	elif random_number == 8:
 		add_child(instance)
 		instance.position = $Location/Spawns/MobSpawn9.position
+		
+
 func _on_restart():
 	get_tree().call_group("ToReset","queue_free")
 	GlobalVars.enemyCount = 0
