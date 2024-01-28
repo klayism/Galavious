adf6b649bebeb2c3b1428c529bb7c8c7b9c03299
klayism <94929502+klayism@users.noreply.github.com>
2024-01-27 22:54:56 -0500

    started adding bomber stuff back

--- project/Code/Main.gd
+++ project/Code/Main.gd
@@ -42,10 +42,11 @@ func _process(delta):
 	$UI/ProgressBar/Percentage.text = "[center]" + str(roundf(($Player/DashCooldown.time_left /  3)*30)/10) + "s"
 	$UI/ScoreBoard.text = "[center]Score: " + str(GlobalVars.score)
 
-func _on_mob_spawn_timeout(): # there has got to be a better way to do this
+func _on_mob_spawn_timeout():
 	var scene = preload("res://Scenes/Enemy.tscn") 
 	var Bomberscene = preload("res://Scenes/Bomber.tscn")
 	var bomberRatio = 0
+	var enemyType
 	if GlobalVars.killedEnemys > 10:
 		bomberRatio = 1
 	elif GlobalVars.killedEnemys > 20:
@@ -59,27 +60,34 @@ func _on_mob_spawn_timeout(): # there has got to be a better way to do this
 		var random_number = randi() % 9
 		if bomberRatio == 0:
 			enemy_instance = scene.instantiate()
+			enemyType = "regular"
 		elif bomberRatio == 1:
 			enemy_instance = Bomberscene.instantiate()
+			enemyType = "bomber"
 		enemy_instance.add_to_group("ToReset")
 		spawnplacement(enemy_instance, random_number)
-		var letimer = firingtimermaker(enemy_instance)
+		var letimer = firingtimermaker(enemy_instance,enemyType)
 		enemy_instance.add_child(letimer)
 		$MobSpawnTimer.start()
 
-func firingtimermaker(enemy_instance):
+func firingtimermaker(enemy_instance,enemyType):
 	var FiringTimer = Timer.new()
 	if is_instance_valid(enemy_instance) == true :
-		FiringTimer.timeout.connect(fire.bind(enemy_instance))
+		FiringTimer.timeout.connect(fire.bind(enemy_instance,enemyType))
 		FiringTimer.wait_time = 1.2
 		FiringTimer.autostart = true
 		return FiringTimer
 	else:
 		FiringTimer.queue_free()
 
-func fire(parentenemy): # there has got to be a better way to do this 
+func fire(parentenemy,enemyType):
+	var instance
 	var scene = preload("res://Scenes/Enemy_Bullet.tscn") 
-	var instance = scene.instantiate()
+	var bomberBulletScene = preload("res://Scenes/Enemy_Bullet.tscn")
+	if enemyType == "regular":
+		instance = scene.instantiate()
+	elif enemyType == "bomber":
+		instance = bomberBulletScene.instantiate()
 	instance.position = parentenemy.position
 	var direction = ($Player.position - parentenemy.position).normalized() 
 	instance.gameovercontrol = Game_over_visibility #allows the bullet to display the game over screen when it hits the player 
